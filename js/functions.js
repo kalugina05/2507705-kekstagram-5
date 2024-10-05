a101796778b21d3867c54b9cdc36499186bf571f
Анна Калугина <a.a,kalugina@urfu.me>
2024-10-05 13:55:17 +0500

    больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,16 @@
-function lineLength(line, maxLength){
-  return (line.length <= maxLength);
-}
+const lineLength = function(line, linelength){
+  return line.length <= linelength;
+};
 
-function isPalindrome (line){
-  let editedLine = (line.replaceAll(' ','')).toLowerCase();
+lineLength('проверка', 10);
+
+const isPalindrome = function(line){
+  const editedLine = (line.replaceAll(' ','')).toLowerCase();
   let newLine = '';
   for (let i = editedLine.length - 1; i >= 0; i--){
     newLine += editedLine[i];
   }
   return (editedLine === newLine);
-}
+};
+
+isPalindrome('тОп от');
